96f341521ffb9af6f920d62fa4b3f0c8d237a0c4
liuyilun2000 <liuyilun2000@126.com>
2024-11-15 16:17:33 +0100

    commonsense

--- commonsense/eval.sh
+++ commonsense/eval.sh
@@ -21,37 +21,6 @@ MODEL_TYPE=$1
 #SBATCH --job-name=OLMoE-1B-7B.${MODEL_TYPE}.eval
 #SBATCH --output=log/OLMoE-1B-7B.${MODEL_TYPE}.%j.eval.slurm.log
 
-# Define the configurations
-declare -a configs=(
-    "lora_4.${MODEL_TYPE}"
-    "lora_8.${MODEL_TYPE}"
-    "lora_16.${MODEL_TYPE}"
-    "lora_32.${MODEL_TYPE}"
-    "lora_64.2-1"
-    "lora_64.2-2"
-    "lora_4.1"
-    "lora_2.e"
-    "lora_128.2-2"
-    "lora_128.4-4"
-    "lora_128.8-8"
-    "lora_64.4-1"
-    "lora_64.e"
-    "lora_64.4-4"
-    "lora_64.8-8"
-    "lora_64.16-8"
-    "lora_4.1-1"
-    "lora_8.1-1"
-    "lora_16.1-1"
-    "lora_32.1-1"
-    "parallel_4.1-1"
-    "parallel_8.1-1"
-    "parallel_16.1-1"
-    "parallel_32.1-1"
-    "parallel_4.2-2"
-    "parallel_8.2-2"
-    "parallel_16.2-2"
-    "parallel_32.2-2"
-)
 '''
 declare -a configs=(
     "parallel_4.4-2"
@@ -71,11 +40,10 @@ run_evaluation() {
     for dataset in "boolq" "piqa" "social_i_qa" "winogrande" "ARC-Challenge" "ARC-Easy" "openbookqa" "hellaswag"; do
         python commonsense_evaluate.py \
             --dataset $dataset \
-            --base_model allenai/OLMoE-1B-7B-0924 \
-            --peft_model checkpoints/OLMoE-1B-7B-0924.${config} \
-            --name OLMoE-1B-7B.${config} \
+            --base_model ${config} \
+            --name ${config} \
             --batch_size 16 --max_new_tokens 4 \
-            | tee -a log/OLMoE-1B-7B.${config}.eval.${dataset}.log
+            | tee -a log/${config}.eval.${dataset}.log
     done
 }
 
